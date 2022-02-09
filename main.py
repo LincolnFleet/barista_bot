704fcf3b4dd5a6bf1a39096b773f78d7f926de28
Travis Martin <travisemerymartin@gmail.com>
2022-02-09 18:20:27 +0000

    Add random inspiring quote

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord as DISCORD
 import os as OS
 import random
+import requests
+import json
 
 client = DISCORD.Client()
 bot_token = OS.environ['DISCORD_BOT_TOKEN']
@@ -21,14 +23,19 @@ indicators_of_sadness = [
 	"failure"
 ]
 
+def get_inspiring_quote():
+	response = requests.get("https://www.zenquotes.io/api/random")
+	data = json.loads(response.text)
+	return data[0]['q'] + " -" + data[0]['a']
+
 @client.event
 async def on_ready():
 	print (f"barista_bot has logged in as {client.user}")
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
+	# Guard: if message author is barista_bot, skip
+	if message.author == client.user : return
 
 	msg = message.content
 
@@ -38,11 +45,11 @@ async def on_message(message):
 			await message.channel.send("Hello back!")
 		if msg[2:].startswith("goodbye"):
 			await message.channel.send("Until next time!")
+		if msg[2:].startswith("inspire"):
+			await message.channel.send(get_inspiring_quote())
 	# tasks which don't require the message to start with trigger
 	else:
 		if any(word in msg for word in indicators_of_sadness):
 			await message.channel.send(random.choice(inspiring_quotes))
-
-	
 
 client.run(bot_token)
